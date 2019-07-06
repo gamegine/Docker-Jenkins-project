9998019d61b35b735132a6b4deb3d865d996378d
gamegine <victor.fromentin95@ynov.com>
2019-07-07 00:36:46 +0800

    run php 7.1 nj 12

---
+++ samplephpwebsite/PHPUnit.php
@@ -0,0 +1,17 @@
+<?php
+require "config.php";
+require "functions.php";
+
+class StackTest extends PHPUnit\Framework\TestCase
+{
+    public function testSiteVersion()
+    {
+        $this->expectOutputString(config('version'));
+        $result =  siteVersion();
+    }
+    public function testSiteName()
+    {
+        $this->expectOutputString(config('name'));
+        $result = siteName();
+    }
+}
